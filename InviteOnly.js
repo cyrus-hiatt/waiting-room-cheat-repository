fdc951bf27743d277e42b205ce0634617c8fafb1
Cyrus Hiatt <cehiatt87@gmail.com>
2024-12-04 14:52:30 +0100

    update to invite only script

--- InviteOnly.js
+++ InviteOnly.js
@@ -1,9 +1,9 @@
 if ($(".key-required").length > 0) {
   if (culture === "nl-NL") {
     if ($("#keyRequiredContainer").length === 0) {
-      $(
-        `<span id="inviteHeader">Momenteel is er een verkoop aan uitkaarthouders. Voer hieronder je e-mail adres in als je een uitkaarthouder bent en een ticket wilt proberen te kopen.</span>`
-      ).appendTo("#header");
+      $(`<span id="inviteHeader">Invite only text.</span>`).appendTo(
+        "#divChallenge_Content"
+      );
     }
   }
 }
